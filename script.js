728335e2237402834d34352fc85b71530336ec22
humancoder1 <jhamanas4532@gmail.com>
2023-10-17 23:37:58 +0530

    created a floater drawing function

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-// //Game-Board Properties
+//Game-Board Properties
+let Game_Board;
 let GB_Width = 800;
 let GB_Height = 800;
-
+let contx;
 
 //Ball properties
 let ball_radius = 50;
@@ -11,25 +12,32 @@ let x_vel = 2;
 let y_vel = 2;
 
 //floater properties
-let f_width = 85;
-let f_height = 10;
+let f_width = 110;
+let f_height = 15;
 let floater = {
     x : GB_Width/2 - f_width/2,
-    y : GB_Height - f_height/2,
+    y : GB_Height - f_height/2 -30,
     width : f_width,
     height : f_height
 }
 
 window.onload =function(){
    
-    const game_Board = document.getElementById("game-Board");
+    Game_Board = document.getElementById("game-Board");
     
     //canvas or game-board dimesions
-    game_Board.width = GB_Width;
-    game_Board.height = GB_Height;
+    Game_Board.width = GB_Width;
+    Game_Board.height = GB_Height;
     
-    const contx = game_Board.getContext("2d");
+    const contx = Game_Board.getContext("2d");
     
+
+    function draw_Floater(){
+        contx.fillStyle = "black";
+        contx.fillRect(floater.x , floater.y , floater.width , floater.height);
+    }
+    draw_Floater();
+
     // function draw_Ball(){
     //     contx.beginPath();
     //     contx.arc(ball_X , ball_Y ,ball_radius , Math.PI * 2);
