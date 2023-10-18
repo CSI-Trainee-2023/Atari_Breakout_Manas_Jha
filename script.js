6e2816737e369c2a8dc03cfb2916addbf0af7435
humancoder1 <jhamanas4532@gmail.com>
2023-10-18 06:26:57 +0530

    Added keydown event for floater movement

--- script.js
+++ script.js
@@ -4,22 +4,28 @@ let GB_Width = 800;
 let GB_Height = 800;
 let contx;
 
-//Ball properties
-let ball_radius = 50;
-let ball_X = GB_Width / 2 ;
-let ball_Y = GB_Height - 30;
-let x_vel = 2;
-let y_vel = 2;
 
 //floater properties
 let f_width = 110;
 let f_height = 15;
+let f_velX = 10;
+
+
 let floater = {
     x : GB_Width/2 - f_width/2,
     y : GB_Height - f_height/2 -30,
     width : f_width,
-    height : f_height
+    height : f_height,
+    vel_X : f_velX
 }
+
+//Ball properties
+let ball_radius = 50;
+let ball_X = GB_Width / 2 ;
+let ball_Y = f_height -10;
+let x_vel = 2;
+let y_vel = 2;
+
 
 window.onload =function(){
    
@@ -36,15 +42,27 @@ window.onload =function(){
         contx.fillStyle = "black";
         contx.fillRect(floater.x , floater.y , floater.width , floater.height);
     }
-    draw_Floater();
+    // draw_Floater();
 
-    // function draw_Ball(){
-    //     contx.beginPath();
-    //     contx.arc(ball_X , ball_Y ,ball_radius , Math.PI * 2);
-    //     contx.fillStyle = "black";
-    //     contx.fill();
-    //     contx.closePath();
-    // }
+    function draw_Ball(){
+        contx.beginPath();
+        contx.arc(ball_X , ball_Y ,ball_radius , Math.PI * 2);
+        contx.fillStyle = "white";
+        contx.fill();
+        contx.closePath();
+    }
+    // draw_Ball();
+
+    document.addEventListener("keydown" , move_floater);
 
 }
 
+function move_floater(event){
+    if (event.code == "ArrowLeft"){
+        floater.x -= floater.vel_X; 
+    }
+
+    else if(event.code == "ArrowRight"){
+        floater.x += floater.vel_X;
+    }
+}
